9323f505fa2ced49bda32390b49d0b58796deb90
cool-Blue <cool.blue@y7mail.com>
2016-12-02 16:44:51 +1100

    Added some extra control types Experimented with different types of event listeners

--- Spec/MainWindow.xaml.cs
+++ Spec/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using ContentToggleButton.ViewModel;
 
 namespace Spec
@@ -22,6 +25,12 @@ namespace Spec
 
 			InitializeComponent();
 
+			Toggle.Loaded += (o, e) =>
+			{
+				Debug.Print("Toggle Loaded");
+				e.Handled = true;
+			};
+
 			//// CLR Bindings
 			ButtonView.Bind(clrToggle,
 				new List<string> { "clr Checked", "clr UnChecked" }, true);
@@ -35,10 +44,34 @@ namespace Spec
 			{
 				clrToggle.IsEnabled = !clrToggle.IsEnabled;
 			};
-			//DefPlain.Click += (s, e) =>
-			//{
-			//	DefToggle.IsEnabled = !DefToggle.IsEnabled;
-			//};
+
+			var cbBound = false;
+			cbButton.Click += (s, e) =>
+			{
+				if (cbBound)
+				{
+					CB.SetCurrentValue(ToggleButton.IsCheckedProperty, null);
+				}
+				//else
+				//{
+				//	CB.ClearValue(ToggleButton.IsCheckedProperty);
+				//}
+				cbBound = !cbBound;
+			};
 		}
+
+		void PanelButtonClick (object o, RoutedEventArgs e)
+		{
+			Debug.Print("PanelButtonClick from {0}\tRouted Event {1}\tSourced from {2}", 
+				o.GetType().Name, e.RoutedEvent, e.Source.GetType().Name);
+		}
+
+		void StyleClick (object o, RoutedEventArgs e)
+		{
+			Debug.Print("StyleClick from {0}\tSourced from {1}", 
+				o.GetType().Name, e.Source.GetType().Name);
+		}
+
 	}
+
 }
